2d4cc7450d20c0ed50faf372d18c74720f870472
Sakurakouji-Saika <fischl@outlook.it>
2023-01-11 00:25:32 +0800

    golang 接口值类型接收者和指针类型接收者

---
+++ 58.golang接口的类型接收者和指针类型接收者.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Pet interface {
+	eat(string) string
+}
+type CatM struct {
+	name string
+}
+
+// func (Cat CatM) eat(name string) string {
+func (Cat *CatM) eat(name string) string {
+	Cat.name = "花花....\n"
+	fmt.Printf("name-eat : %v \n", name)
+	return "吃的很好\n"
+}
+
+func main() {
+	// cat := CatM{
+	cat := &CatM{
+		name: "画画...\n",
+	}
+
+	s := cat.eat("哗哗...\n")
+
+	fmt.Printf("s: %v \n", s)
+	fmt.Printf("dog: %v \n", cat)
+}
